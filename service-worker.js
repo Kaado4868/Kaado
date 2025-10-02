af2297d3d253d386019cbb6aef62c5506deb3674
Kaado4868 <127008565+Kaado4868@users.noreply.github.com>
2025-10-02 05:52:38 +0100

    Updated index

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,22 @@
-const CACHE_NAME="secret-cipher-v1";
-const ASSETS=["/","/index.html","/manifest.json","/icon-192.png","/icon-512.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))) });
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request))) });
+const CACHE_NAME = "secret-cipher-v3";
+const ASSETS = ["/", "/index.html", "/quiz.html", "/manifest.json", "/icon-192.png", "/icon-512.png", "/anime.min.js", "/service-worker.js"];
+
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", e => {
+  clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
