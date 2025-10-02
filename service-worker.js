9e1e293787633a7c72b01167cdb9cf17423e1f8f
Kaado4868 <127008565+Kaado4868@users.noreply.github.com>
2025-10-02 14:23:08 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,25 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('cipher-cache').then((cache) => cache.addAll([
-      'index.html','quiz.html','manifest.json','icon-192.png','icon-512.png'
-    ]))
+const CACHE_NAME = "cipher-cache-v3";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/service-worker.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(names => Promise.all(names.map(name => { if(name !== CACHE_NAME) return caches.delete(name); })))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
